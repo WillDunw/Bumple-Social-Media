3f0ec1cf3ec96ac7c2cc7f46bae3c738daf4d534
WillDunw123 <98476823+WillDunw@users.noreply.github.com>
2023-10-07 14:55:14 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.0" apply false
 }
-
-ktlint {
-    version = "0.42.1" // Use the desired ktlint version
-    ignoreFailures = false // Set to true if you want to continue even if there are ktlint issues
-    reporters {
-        reporter("plain")
-        reporter("checkstyle")
-    }
-}
