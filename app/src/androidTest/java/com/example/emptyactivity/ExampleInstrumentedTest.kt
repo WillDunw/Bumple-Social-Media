7dc6fdccffe42af096560dcf71bc8d04f9d9bacc
WillDunw123 <98476823+WillDunw@users.noreply.github.com>
2023-10-07 14:07:49 -0400

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/emptyactivity/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/emptyactivity/ExampleInstrumentedTest.kt
@@ -2,9 +2,9 @@ package com.example.emptyactivity
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.runner.RunWith
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.Assert.assertEquals
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
