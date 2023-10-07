4bd90a4b75faa285865fea779ffd8f3e333b569a
WillDunw123 <98476823+WillDunw@users.noreply.github.com>
2023-10-07 14:29:06 -0400

    commenting out file because it makes no sense

--- app/src/androidTest/java/com/example/emptyactivity/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/emptyactivity/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.emptyactivity
+/*package com.example.emptyactivity
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
