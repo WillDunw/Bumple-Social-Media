2d51dec8328f0e135f997098f2f7c9aff7f72ac4
WillDunw123 <98476823+WillDunw@users.noreply.github.com>
2023-10-07 13:59:43 -0400

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/emptyactivity/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/emptyactivity/ExampleInstrumentedTest.kt
@@ -2,8 +2,8 @@ package com.example.emptyactivity
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
 import org.junit.runner.RunWith
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
