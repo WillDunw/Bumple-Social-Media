c42037f44569078c69b68fb70dc5697c01cdcddb
William Dunwoody <williamdunwoody1@gmail.com>
2023-11-09 17:09:11 -0500

    added filters for home page post fo flter the controversy on posts and to not show posts you've already seen(or try to)

--- app/src/main/java/com/example/emptyactivity/DataModels/PostViewModel.kt
+++ app/src/main/java/com/example/emptyactivity/DataModels/PostViewModel.kt
@@ -2,6 +2,7 @@ package com.example.emptyactivity.DataModels
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
