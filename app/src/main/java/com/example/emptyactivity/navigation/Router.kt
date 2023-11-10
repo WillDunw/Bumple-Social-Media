1dc8e1e438c4125dc989376dfdcb419274f9a929
brandon <brandon.khublall@hotmail.com>
2023-11-10 02:33:55 -0500

    text box wont work...

--- app/src/main/java/com/example/emptyactivity/navigation/Router.kt
+++ app/src/main/java/com/example/emptyactivity/navigation/Router.kt
@@ -10,10 +10,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.emptyactivity.DataModels.PostViewModel
+import com.example.emptyactivity.HomeRoutes
+import com.example.emptyactivity.LoginRoutes
 import com.example.emptyactivity.Pages.CreatePost
 import com.example.emptyactivity.Pages.Home
 import com.example.emptyactivity.Pages.SignIn
 import com.example.emptyactivity.Pages.ViewAccount
+import com.example.emptyactivity.login.LoginScreen
+import com.example.emptyactivity.login.SignUpScreen
 
 val LocalNavController = compositionLocalOf<NavController> { error("No nav controller found :(")  }
 
@@ -23,7 +27,7 @@ fun Router(postViewModel : PostViewModel){
     val navController = rememberNavController()
 
     CompositionLocalProvider(LocalNavController provides navController) {
-        NavHost(navController = navController, startDestination = Routes.SignUp.route) {
+        NavHost(navController = navController, startDestination = LoginRoutes.SignIn.name) {
             composable(Routes.Home.route){
                 Home(postViewModel)
             }
@@ -36,8 +40,42 @@ fun Router(postViewModel : PostViewModel){
                 ViewAccount(postViewModel)
             }
 
-            composable(Routes.SignUp.route){
-                SignIn()
+//            composable(Routes.SignUp.route){
+//                SignIn()
+//            }
+
+            composable(route = LoginRoutes.SignIn.name){
+                LoginScreen(onNavToHomePage = {
+                    navController.navigate(HomeRoutes.Home.name){
+                        launchSingleTop = true
+                        popUpTo(route = LoginRoutes.SignIn.name){
+                            inclusive = true
+                        }
+                    }
+                }) {
+                    navController.navigate(LoginRoutes.SignUp.name){
+                        launchSingleTop = true
+                        popUpTo(route = LoginRoutes.SignIn.name){
+                            inclusive = true
+                        }
+                    }
+                }
+            }
+
+            composable(route = LoginRoutes.SignUp.name){
+                SignUpScreen(onNavToHomePage ={
+                    navController.navigate(HomeRoutes.Home.name){
+                        popUpTo(route = LoginRoutes.SignUp.name){
+                            inclusive = true
+                        }
+                    }
+                }){
+                    navController.navigate(LoginRoutes.SignIn.name)
+                }
+            }
+
+            composable(route = HomeRoutes.Home.name){
+                // TODO: 11/10/2023
             }
         }
     }
